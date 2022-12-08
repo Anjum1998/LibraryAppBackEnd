e37e34e88a2c722c7c800258908dcebbec837b95
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-12-08 21:29:00 +0530

    add

--- src/main/java/com/example/LibraryApp_Backend/Controller/LibraryController.java
+++ src/main/java/com/example/LibraryApp_Backend/Controller/LibraryController.java
@@ -1,12 +1,17 @@
 package com.example.LibraryApp_Backend.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.LibraryApp_Backend.Controller.model.Library;
+import com.example.LibraryApp_Backend.dao.LibraryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LibraryController {
-
+    @Autowired
+    private LibraryDao dao;
     @PostMapping("/")
     public String AdminLogin()
     {
@@ -22,10 +27,23 @@ public class LibraryController {
     {
         return "Welcome to user register page";
     }
-    @PostMapping("/entry")
-    public String BookEntry()
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/entry",consumes = "application/json",produces = "application/json")
+    public Map<String,String> BookEntry(@RequestBody Library l)
     {
-        return "Welcome to book entry page";
+        System.out.println(l.getTitle().toString());
+        System.out.println(l.getAuthor().toString());
+        System.out.println(l.getDescription().toString());
+        System.out.println(l.getDistributor().toString());
+        System.out.println(l.getLanguage().toString());
+        System.out.println(l.getPrice());
+        System.out.println(l.getPublisher().toString());
+        System.out.println(l.getImage().toString());
+        System.out.println(l.getReleaseyear());
+        dao.save(l);
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
     }
     @PostMapping("/search")
     public String BookSearch()
